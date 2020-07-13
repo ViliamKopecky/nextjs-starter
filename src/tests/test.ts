1d2edc621676645d211002a45906adfe0b349e1b
Viliam Kopecky <vk@viliamkopecky.cz>
2020-07-13 10:39:33 +0200

    eslint & prettier

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -1,8 +1,8 @@
-import { test } from 'uvu';
-import * as assert from 'uvu/assert';
+import { test } from 'uvu'
+import * as assert from 'uvu/assert'
 
 test('test should always pass', () => {
-  assert.is(true, true);
-});
+	assert.is(true, true)
+})
 
-test.run();
+test.run()
